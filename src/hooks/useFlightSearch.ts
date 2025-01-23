6c10c192167d9a6e034363e26746a01f0786d70d
meshach567 <arinzemeshach123@gmail.com>
2025-01-23 18:45:35 +0100

    refactor: removed unnecessary html element

---
+++ src/hooks/useFlightSearch.ts
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import {useQuery} from 'react-query';
+
+const useFlightSearch = () => {
+  return (
+    <div>useFlightSearch</div>
+  )
+}
+
+export default useFlightSearch
