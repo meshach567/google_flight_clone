5e533ae8bfa155f63c49de84d3328266f4babbce
meshach567 <arinzemeshach123@gmail.com>
2025-01-23 16:33:10 +0100

    introduce-feat: introduce commitlint to the code base

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    "type-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      ["ci", "feat", "fix", "refactor", "revert", "doc", "style", "chore", "test"],
+    ],
+  }
+
+}
