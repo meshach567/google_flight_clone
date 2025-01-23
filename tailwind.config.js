c67a57527a0973ac27c935686e58f9efc8a6971b
meshach567 <arinzemeshach123@gmail.com>
2025-01-23 16:47:49 +0100

    style: introduce tailwind config to rootfolder

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          'flight-primary': '#2C3E50',
+          'flight-secondary': '#3498DB'
+        }
+      },
+    },
+    plugins: [],
+  };
