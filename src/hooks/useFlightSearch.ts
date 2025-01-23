ee19a62e733283f423d574fffb1e1221f96913bf
meshach567 <arinzemeshach123@gmail.com>
2025-01-23 18:59:09 +0100

    refactor: rafator the useFlightSearch file

--- src/hooks/useFlightSearch.ts
+++ src/hooks/useFlightSearch.ts
@@ -1,10 +1,26 @@
-import React, { useState } from 'react';
-import {useQuery} from 'react-query';
+import  { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { GoogleFlight, GoogleFlightSearchParams } from '../types/googleflight';
+import { searchGoogleFlights } from '../services/flightApi';
 
 const useFlightSearch = () => {
-  return (
-    <div>useFlightSearch</div>
-  )
+  const [searchParams, setSearchParams] = useState<GoogleFlightSearchParams | null>(null);
+  const { data: flights, isLoading, error, refetch } = useQuery<GoogleFlight[]>({
+    queryKey: ['flights', searchParams],
+    queryFn: () =>  searchGoogleFlights(searchParams!),
+      enabled: !!searchParams
+});
+
+  const performSearch = (params: GoogleFlightSearchParams) => {
+    setSearchParams(params);
+    refetch();
+  }
+  return { 
+    flights, 
+    isLoading, 
+    error, 
+    performSearch 
+  };
 }
 
 export default useFlightSearch
